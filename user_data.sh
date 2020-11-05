904e8d530a355a3a589db2ca2744f816e349b967
Simon Chu <simon_chu@kelaberetiv.sg>
2020-11-05 23:43:13 +0800

    WIP: still could properly install the packages

--- user_data.sh
+++ user_data.sh
@@ -2,6 +2,8 @@
 yum update -y
 yum install -y python3
 yum install -y git
-pip install pandas
-pip install streamlit
-git clone 
+pip3 install --upgrade -r requirements.txt
+
+git clone https://github.com/simonchuth/seq_listing_patent_tool.git /home/ec2-user
+iptables -t nat -A PREROUTING -i eth0 -p tcp --dport 80 -j REDIRECT --to-port 8501
+streamlit run /seq_listing_patent_tool/src/app.py
