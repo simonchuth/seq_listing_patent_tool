7c1ac162fe033be57c10a4e693131586c0f27578
Simon Chu <simon_chu@kelaberetiv.sg>
2020-11-06 23:22:19 +0800

    Add process_mod and check_cds

--- src/app.py
+++ src/app.py
@@ -8,6 +8,9 @@ import json
 import streamlit as st
 import pandas as pd
 
+from parameters import valid_nucleotide
+from sample_data import sample_dict
+
 
 def download_link(object_to_download, download_filename, download_link_text):
     """
@@ -30,42 +33,90 @@ def download_link(object_to_download, download_filename, download_link_text):
 
     return f'<a href="data:file/txt;base64,{b64}" download="{download_filename}">{download_link_text}</a>'
 
-def process_row(row_text):
-    row_text = row_text + '\n'
-    return row_text
-
 def seq_row(seq_chunk):
     chunklist = [seq_chunk[x:x + 10] for x in range(0, len(seq_chunk), 10)]
     seq_chunk_separated = ' '.join(chunklist)
     return seq_chunk_separated
 
-def check_invalid_seq(seq):
-    seq = seq
-    valid_nucleotide = set(['a',
-                      'g',
-                      'c',
-                      't',
-                      'u',
-                      'r',
-                      'y',
-                      'm',
-                      'k',
-                      's',
-                      'w',
-                      'b',
-                      'd',
-                      'h',
-                      'v',
-                      'n'])
+
+def check_invalid_seq(seq, mod_dict=None, seq_type='DNA'):
+
+    seq_type = seq_type.lower()
+
+    if (seq_type == 'dna') or (seq_type == 'rna'):
+        valid_set = valid_nucleotide.copy()
+
+    if mod_dict is not None:
+        mod_set = set(mod_dict.keys())
+        valid_set = valid_set.union(mod_set)
+
     for i, char in enumerate(seq):
-        if char not in valid_nucleotide:
+        if char not in valid_set:
             return [i, char]
+
     return None
+
+
+def process_mod(mod):
+    if mod != 'nan':
+        mod_list = mod.split(';')
+        mod_dict = {}
+        for mod_entry in mod_list:
+            mod_entry = mod_entry.split('=')
+            mod_dict[mod_entry[0].strip(' ')] = mod_entry[1].strip(' ')
+        return mod_dict
+
+    else:
+        return None
 
 def check_nan(display_idx, dict_str):
     for key, val in dict_str.items():
         if val == 'nan':
-            st.warning(f'Sequence in row {display_idx} contain missing value for {key}')
+            st.warning(f'Sequence in row {display_idx} contain missing value \
+                for {key}')
+
+
+def check_cds(display_idx, cds):
+    if cds  != 'nan':
+        cds_split = cds.split(',')
+        if len(cds_split) != 2:
+            st.warning(f'CDS_Range in row {display_idx} is of incorrect \
+                format ({cds}), it should be 2 numbers separated by a comma.\
+                 e.g. 21,51')
+            return None
+
+        start = int(cds_split[0])
+        end = int(cds_split[1])
+        CDS_len = end - start + 1
+        if CDS_len % 3 != 0:
+            st.warning(f'CDS_Range in row {display_idx} is invalid. The range \
+                should be in multiple of 3, {start} to {end} is not in \
+                multiple of 3')
+            return None
+
+        return [start, end]
+
+    else:
+        return None
+
+
+def add_line(output_str, content, num_id=None, num_blank_lines=0):
+    line_str = ''
+
+    if not isinstance(content, str):
+        content = str(content)
+
+    if num_id is not None:
+        line_str = line_str + f'<{num_id}>  '
+
+    line_str = line_str + content + ' \n'
+
+    # Add blank lines
+    line_str = line_str + ' \n' * num_blank_lines
+
+    output_str = output_str + line_str
+
+    return output_str
 
 st.title('Sequence Listing Tool')
 
@@ -75,14 +126,10 @@ inv_title = st.text_input('Title of Invention')
 
 file_ref = st.text_input('File Reference')
 
-sample_seq = ['ATCGATCGATCG', 'AUGCGUACGUACGUAC']
-sample_type = ['DNA','RNA']
-sample_org = ['Homo sapiens', 'Homo sapiens']
-sample_dict = {'Sequence': sample_seq,
-               'Sample Type': sample_type,
-               'Organism': sample_org}
 df = pd.DataFrame(sample_dict)
-tmp_download_link = download_link(df, 'template.csv', 'Template file for sequence listing')
+tmp_download_link = download_link(df,
+                                  'template.csv',
+                                  'Template file for sequence listing')
 st.markdown(tmp_download_link, unsafe_allow_html=True)
 uploaded_file = st.file_uploader("Choose a file", type=['csv'])
 
@@ -90,50 +137,67 @@ if uploaded_file is not None:
     uploaded_file.seek(0)
     df = pd.read_csv(uploaded_file)
 
-seq_list = df.iloc[:, 0].tolist()
-type_list = df.iloc[:, 1].tolist()
-org_list = df.iloc[:, 2].tolist()
+seq_list = df['Sequence'].tolist()
+type_list = df['Sample_Type'].tolist()
+org_list = df['Organism'].tolist()
+cds_list = df['CDS_Range'].tolist()
+mod_list = df['Modified_Legend'].tolist()
 
 num_entry = len(seq_list)
 
-output_str = ''
+output_str ='                         SEQUENCE LISTING\n \n'
 
 if st.button('Generate sequence listing in txt'):
-    output_str = output_str + process_row('                         SEQUENCE LISTING')
-    output_str = output_str + process_row('')
-    output_str = output_str + process_row('<110>  ' + app_name)
-    output_str = output_str + process_row('')
-    output_str = output_str + process_row('<120>  ' + inv_title)
-    output_str = output_str + process_row('')
-    output_str = output_str + process_row('<130>  ' + file_ref)
-    output_str = output_str + process_row('')
-    output_str = output_str + process_row('<160>  ' + str(num_entry))
-    output_str = output_str + process_row('')
+    output_str = add_line(output_str, app_name, num_id=110, num_blank_lines=1)
+    output_str = add_line(output_str, inv_title, num_id=120, num_blank_lines=1)
+    output_str = add_line(output_str, file_ref, num_id=130, num_blank_lines=1)
+    output_str = add_line(output_str, num_entry, num_id=160, num_blank_lines=1)
+
     for idx in range(num_entry):
         display_idx = idx + 1
         seq = str(seq_list[idx])
         seq_type = str(type_list[idx])
         org = str(org_list[idx])
+        cds = str(cds_list[idx])
+        mod = str((mod_list[idx]))
+
         check_nan(display_idx, {'Sequence': seq,
                                 'Sequence Type': seq_type,
                                 'Organism': org})
 
+        cds = check_cds(display_idx, cds)
+
+        mod_dict = process_mod(mod)
+
         seq = seq.replace(' ', '').lower()
-        invalid_seq_status = check_invalid_seq(seq)
+        invalid_seq_status = check_invalid_seq(seq, mod_dict=mod_dict, seq_type=seq_type)
         if invalid_seq_status is not None:
-            st.warning(f'Sequence in row {display_idx} contain invalid character ({invalid_seq_status[1]}) at position ({invalid_seq_status[0]})')
+            st.warning(f'Sequence in row {display_idx} contain invalid \
+                character ({invalid_seq_status[1]}) at position \
+                ({invalid_seq_status[0]})')
+        
         seq_len = len(seq)
 
+        output_str = add_line(output_str, display_idx, num_id=210, num_blank_lines=0)
+        output_str = add_line(output_str, seq_len, num_id=211, num_blank_lines=0)
+        output_str = add_line(output_str, seq_type, num_id=212, num_blank_lines=0)
+        output_str = add_line(output_str, org, num_id=213, num_blank_lines=1)
 
-        num_seq_row = math.ceil(seq_len / 60)
+        if mod_dict is not None:
+            for key, feature in mod_dict.items():
+                for i, char in enumerate(seq):
+                    if char == key:
+                        location = i + 1
+                        output_str = add_line(output_str, '', num_id=220, num_blank_lines=0)
+                        output_str = add_line(output_str, 'misc_feature', num_id=221, num_blank_lines=0)
+                        output_str = add_line(output_str, f'({location})..({location})', num_id=222, num_blank_lines=0)
+                        output_str = add_line(output_str, f'n = {feature}', num_id=223, num_blank_lines=1)
+                seq = seq.replace(key, 'n')
+
+        output_str = add_line(output_str, display_idx, num_id=400, num_blank_lines=0)
+
         chunk_seq_list = [seq[x:x + 60] for x in range(0, seq_len, 60)]
 
-        output_str = output_str + process_row('<210>  ' + str(display_idx))
-        output_str = output_str + process_row('<211>  ' + str(seq_len))
-        output_str = output_str + process_row('<212>  ' + seq_type)
-        output_str = output_str + process_row('<213>  ' + org)
-        output_str = output_str + process_row('')
-        output_str = output_str + process_row('<400>  ' + str(display_idx))
         for i, seq_chunk in enumerate(chunk_seq_list):
             seq_chunk_separated = seq_row(seq_chunk)
             chunk_len = len(seq_chunk)
@@ -141,10 +205,13 @@ if st.button('Generate sequence listing in txt'):
             line_count_str = str(line_count)
             pad_len = 74 - len(seq_chunk_separated) - len(line_count_str)
             pad = ' ' * pad_len
-            output_str = output_str + process_row(seq_chunk_separated + pad + line_count_str)
-        output_str = output_str + process_row('')
+            line_output = seq_chunk_separated + pad + line_count_str
+            output_str = add_line(output_str, line_output, num_id=None, num_blank_lines=1)
+        output_str = add_line(output_str, '', num_id=None, num_blank_lines=0)
 
-    tmp_download_link = download_link(output_str, 'Sequence_listing.txt', 'Click here to download your sequence listing!')
+    tmp_download_link = download_link(output_str,
+                                      'Sequence_listing.txt',
+                                      'Click here to download your sequence listing!')
     st.markdown(tmp_download_link, unsafe_allow_html=True)
 
 
