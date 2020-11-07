972012d3e4b8d4fe78c2357ddc18f4a5c2e595e2
Simon Chu <simon_chu@kelaberetiv.sg>
2020-11-07 20:53:00 +0800

    add sample data for amino acid

--- src/sample_data.py
+++ src/sample_data.py
@@ -1,11 +1,14 @@
 import numpy as np
 
 sample_seq = ['ATCATGATGCGATGCTA1CAA2GATCA@GATGCGATGCTAGCAATGCGATGCTAGCATGACGGCAGGGCGATGAAGACGATGCTAGCATGAATCATGATGCGATGCTAGCAATGCGATGCTAGCATGACGGCAGGGCGATGAAGACGGCAGGGCGATGAAGATCGATCG',
-              'AUGCGUACATGCGAATGCGATGCTAGCATGACGGCAGGGCGATGAAGACGATGCTAGCATTGCTAGCAATGCGATGCTAGCATGACGGCAGGGCGATGAAGACGATGCTAGCATGUACGUAC']
-sample_type = ['DNA','RNA']
-sample_org = ['Homo sapiens', 'Homo sapiens']
-sample_cds = [np.nan, '25, 51']
-sample_mod = ['1=ABC; 2=DEF; @=XYZ or ABC', np.nan]
+              'AUGCGUACATGCGAATGCGATGCTAGCATGACGGCAGGGCGATGAAGACGATGCTAGCATTGCTAGCAATGCGATGCTAGCATGACGGCAGGGCGATGAAGACGATGCTAGCATGUACGUAC',
+              'Ala Cys Asp Glu Phe Gly His Ile Lys Leu Met Ala Cys Asp Glu Phe Gly His Ile Lys Leu Met Ala Cys Asp Glu Phe Gly His Ile Lys Leu Met Ala Cys Asp Glu Phe Gly His Ile Lys Leu Met',
+              'TYAGVLYERDVLYERGVLYERDYERGV',
+              'TYAG!VL@YER#DVLYERGVLYERDYERGV']
+sample_type = ['DNA','RNA', 'PRT', 'PRT_1', 'PRT_1']
+sample_org = ['Homo sapiens', 'Homo sapiens', 'Homo sapiens', 'Homo sapiens', 'Homo sapiens']
+sample_cds = [np.nan, '25, 51', np.nan, np.nan, np.nan]
+sample_mod = ['1=ABC; 2=DEF; @=XYZ or ABC', np.nan, np.nan, np.nan, '!=Positive amino acid; @=Negative amino acid; #=Hydrophobic amino acid']
 sample_dict = {'Sequence': sample_seq,
                'Sample_Type': sample_type,
                'Organism': sample_org,
